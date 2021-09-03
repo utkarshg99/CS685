c1b909b2e562e39e4545d168e5ae8fa9951562bc
Utkarsh Gupta <utkarshg99@gmail.com>
2021-09-04 00:54:06 +0530

    q3

--- Assignment 1/q3.py
+++ Assignment 1/q3.py
@@ -6,8 +6,33 @@ with open('out/neighbor-districts-modified.json') as ndjson:
 with open('meta/dist_name_key.json') as dsnk:
     distnamekey = json.load(dsnk)
 
-with open('district.csv', newline='') as dswcsv:
+convt_data={}
+lst = []
+lst_tmp = []
+for k in distnamekey.keys():
+    convt_data[k] = {}
+
+with open('districts.csv', newline='') as dswcsv:
     csvDict = csv.DictReader(dswcsv)
     for row in csvDict:
         if(row["District"] != "Unknown"):
-            pass
+            if row["District"].lower().replace(" ", "_") in distnamekey.keys():
+                convt_data[row["District"].lower().replace(" ", "_")][row["Date"]] = {
+                    "con":row["Confirmed"],
+                    "rec":row["Recovered"],
+                    "det":row["Deceased"],
+                    "oth":row["Other"]
+                }
+            else:
+                lst.append(row["District"].lower().replace(" ", "_"))
+
+for k in distnamekey.keys():
+    if convt_data[k] == {}:
+        lst_tmp.append(k)
+
+with open("meta/q3_log.txt", "w") as mq3:
+    mq3.write(str(set(lst))+"\n")
+    mq3.write(str(lst_tmp)+"\n")
+
+with open("meta/covid_cases.json", "w") as ccjs:
+    json.dump(convt_data, ccjs, indent="\t")
