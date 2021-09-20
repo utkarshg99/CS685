f0fac2be68aa59f3b3be7e79e05735ab091af83b
Utkarsh Gupta <utkarshg99@gmail.com>
2021-09-20 10:53:37 +0530

    added script for q1

--- Assignment 1/edge-generator.sh
+++ Assignment 1/edge-generator.sh
@@ -1,9 +1,2 @@
-mkdir -p meta
-mkdir -p out
-# mkdir -p dump
-python3 meta.py
-echo "Generated Metadata"
-python3 q1.py
-echo "Generated out/neighbor-districts-modified.json"
 python3 q2.py
 echo "Generated out/edge-graph.csv"
